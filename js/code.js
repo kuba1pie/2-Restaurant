61e46b1462a0bd18e0822b52cf3d3536f8498e72
kuba1pie <kuba1pie@gmail.com>
2019-11-06 14:36:18 +0100

    mobile design

--- js/code.js
+++ js/code.js
@@ -14,8 +14,16 @@ function hamburger() {
 }
 function hamWrapper(){
     let el = document.getElementById("hamWrapper")
-    let para = document.createElement("div");               // Create a <p> element
-    para.classList.add("hamItem", "right")
-    para.innerHTML = "<h4>Hamburger</h4><h5>Big and Meaty</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac feugiat sapien, a dapibus lacus.</p><span class='price'>$34.00</span><img src='img/burger1.png' alt='burger'>";               // Insert text
-    el.appendChild(para);                      // Append <p> to <body> 
+    let para = document.createElement("div");   // Create a <p> element
+    // Start Variables
+    let name = "Hamburger"
+    let cla ="right"
+    let title = "Big and Meaty"
+    let description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac feugiat sapien, a dapibus lacus."
+    let photo = "burger1.png"
+    let price="34.00"
+    // End Variables
+    para.classList.add("hamItem", cla)
+    para.innerHTML = "<h4>"+name+"</h4><h5>"+title+"</h5><p>"+description+"</p><span class='price'>$"+price+"</span><img src='img/"+photo+"' alt='"+name+" photo'>";    // Insert content
+    el.appendChild(para);   // Append <p> to <body> 
 }
